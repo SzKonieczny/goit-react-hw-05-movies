f63c29eb865c0022a1074b30190c33941a0bd362
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-11-13 22:55:39 +0100

    apiTools added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import { Layout } from './Layout/Layout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Home = lazy(() =>
+  import('../pages/Home/Home').then(module => ({
+    ...module,
+    default: module.Home,
+  }))
+);
+const Movies = lazy(() =>
+  import('../pages/Movies/Movies').then(module => ({
+    ...module,
+    default: module.Movies,
+  }))
+);
+const MovieDetails = lazy(() =>
+  import('../pages/MovieDetails/MovieDetails').then(module => ({
+    ...module,
+    default: module.MovieDetails,
+  }))
+);
+const Cast = lazy(() =>
+  import('./Cast/Cast').then(module => ({ ...module, default: module.Cast }))
+);
+const Reviews = lazy(() =>
+  import('./Reviews/Reviews').then(module => ({
+    ...module,
+    default: module.Reviews,
+  }))
+);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/" element={<Movies />} />
+          <Route path="/movies/:Id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+      <ToastContainer autoClose={2500} position="top-left" theme="colored" />
     </div>
   );
 };
---
+++ src/components/apiTools/Api.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+const API_KEY = '129ca5a87012be791cb78f7f53b5aff0';
+const BASE_URL = 'https://api.themoviedb.org/3/trending/all/day';
+const URL_GENRES = 'https://api.themoviedb.org/3/genre/movie/list';
+const URL_CAST = 'https://api.themoviedb.org/3/movie/';
+const URL = 'https://api.themoviedb.org/3/';
+
+export default async function fetchTrending() {
+  const config = {
+    method: 'get',
+    url: BASE_URL,
+    params: {
+      api_key: API_KEY,
+    },
+  };
+
+  const response = await axios(config);
+
+  const data = response.data.results;
+  return data;
+}
+
+export async function fetchGenres() {
+  const url = `${URL_GENRES}&language=en-US&api_key=${API_KEY}`;
+
+  const response = await axios(url);
+
+  const data = response.data.genres;
+  return data;
+}
+
+export async function fetchCast(Id) {
+  const url = `${URL_CAST}${Id}/credits?api_key=${API_KEY}&language=en-US`;
+
+  const response = await axios(url);
+
+  const data = response.data.cast;
+  return data;
+}
+
+export async function fetchReviews(Id) {
+  const url = `${URL_CAST}${Id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
+
+  const response = await axios(url);
+
+  const data = response.data.results;
+  return data;
+}
+
+export async function fetchSearchFilm(quary) {
+  const url = `${URL}search/movie?api_key=${API_KEY}&language=en-US&query=${quary}&page=1&include_adult=false`;
+
+  const response = await axios(url);
+
+  const data = response.data.results;
+  return data;
+}
+
+export async function ApiMovieInfo(Id) {
+  const url = `${URL}movie/${Id}?api_key=${API_KEY}&language=en-US`;
+
+  const response = await axios(url);
+
+  const data = response.data;
+  return data;
+}
