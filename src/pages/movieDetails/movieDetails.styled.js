583119628ae840a6182346ace9cb6793416e15be
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-11-16 13:31:44 +0100

    movieDetails updated

---
+++ src/pages/movieDetails/movieDetails.styled.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const FimlContainer = styled.div`
+  display: flex;
+  margin: 10px;
+`;
+export const FilmImgDiv = styled.div`
+  padding: 10px;
+`;
+
+export const InformationDiv = styled.div`
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+`;
